be65a59aa2ea600df4ca9d150a9ae6b0041198a0
mohamed anwer yahyaoui <anweryahyaoui379@gmail.com>
2025-02-17 17:39:06 +0100

    the frontEnd for our SmartDelivery application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './frontOffice/home/home.component';
+import { SigninComponent } from './authentification/signin/signin.component';
+import { SignupComponent } from './authentification/signup/signup.component';
+import { WelcomNewUserComponent } from './authentification/welcom-new-user/welcom-new-user.component';
+import { ForgotPasswordComponent } from './authentification/forgot-password/forgot-password.component';
+import { DashHomeComponent } from './backOffice/dash-home/dash-home.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   // Default Redirect to Client Home
+ { path: '', redirectTo: '/client', pathMatch: 'full' },
+
+ // FrontOffice Layout
+ {
+   path: 'client',
+   component: HomeComponent,
+   children: [
+     { path: '', component: HomeComponent },
+   
+   ],
+ },
+
+ // Authentication
+ { path: 'signin', component: SigninComponent },
+ { path: 'signup', component: SignupComponent },
+ { path: 'welcome', component: WelcomNewUserComponent },
+ { path: 'forgotPassword', component: ForgotPasswordComponent },
+
+ // BackOffice Layout (Dashboard)
+ {
+   path: 'dashboard',
+   component: DashHomeComponent,
+   children: [
+     { path: '', component: DashHomeComponent },
+     
+   ],
+ },
+
+ // Not Found Page
+ { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomNewUserComponent } from './authentification/welcom-new-user/welcom-new-user.component';
+import { SigninComponent } from './authentification/signin/signin.component';
+import { SignupComponent } from './authentification/signup/signup.component';
+import { ForgotPasswordComponent } from './authentification/forgot-password/forgot-password.component';
+import { HomeComponent } from './frontOffice/home/home.component';
+import { DashHomeComponent } from './backOffice/dash-home/dash-home.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomNewUserComponent,
+    SigninComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    HomeComponent,
+    DashHomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
